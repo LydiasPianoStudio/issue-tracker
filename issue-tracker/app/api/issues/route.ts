e8f46b3024f0a66a2d9d9098c6f4293253c4e0ad
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2024-09-29 13:58:10 -0400

    Implement client-side validation

--- issue-tracker/app/api/issues/route.ts
+++ issue-tracker/app/api/issues/route.ts
@@ -1,13 +1,9 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import {z} from "zod";
 import prisma from "@/prisma/client";
+import { createIssueSchema} from "../../validationSchema"
 
 
-const createIssueSchema = z.object({
-    title: z.string().min(1, "Title is required.").max(255),
-    description: z.string().min(1, "Description is required.").max(255)
-});
 
  export async function POST(request: NextRequest) {
     const body = await request.json();
